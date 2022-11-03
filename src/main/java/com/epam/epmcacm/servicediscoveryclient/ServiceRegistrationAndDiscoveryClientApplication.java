8f1f465693ba70c341a607cc047a4a5676e9e4d3
eefbf81 <branko.rovcanin@mckesson.com>
2022-11-03 09:10:10 +0100

    Cleanup and containerization

--- src/main/java/com/epam/epmcacm/servicediscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
+++ src/main/java/com/epam/epmcacm/servicediscoveryclient/ServiceRegistrationAndDiscoveryClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceRegistrationAndDiscoveryClientApplication {
 
 	public static void main(String[] args) {
